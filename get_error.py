8ffb7573540be29dca8f9ac286a3930c0ff04947
Bode Hoover <77543359+bhoover59@users.noreply.github.com>
2023-09-09 11:12:26 -0400

    Update get_error.py
    Uses length of column now instead of constant diurnal (24)

--- get_error.py
+++ get_error.py
@@ -1,7 +1,8 @@
 def get_error(df, col_name):
     '''Get standard error of the mean (SEM) for input column (string)'''
     sigma = df[col_name].std()
-    SEM = sigma / np.sqrt(24)
+    N = len(df[col_name])
+    SEM = sigma / np.sqrt(N)
     error_low = df[col_name] - SEM
     error_high = df[col_name] + SEM
     return error_low, error_high, SEM, sigma
